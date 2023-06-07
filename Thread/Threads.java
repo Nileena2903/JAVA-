4513e1db6bed1b31e6c4dab92d4121221cc42e0c
Lycka <116252133+Nileena2903@users.noreply.github.com>
2023-06-07 12:21:33 +0530

    Add files via upload

---
+++ Thread/Threads.java
@@ -0,0 +1,72 @@
+
+import java.util.Scanner;
+
+class Multiplication extends Thread
+{
+public void run()
+{
+try
+{
+for(int i=0;i<=10;i++)
+{
+System.out.println(i+"x5:"+(i*5));
+Thread.sleep(500);
+}
+}
+catch(InterruptedException e)
+{
+System.out.println("\nMultiplication thread Interrupted");
+}
+}
+}
+class Prime extends Thread
+{
+int limit,count,flag,i,j;
+Prime(int limit)//constructor
+{
+this.limit=limit;
+}
+public void run()
+{
+try
+{
+int j=2;
+while(count<limit)
+{
+flag=1;
+for(i=2;i<=(j-1);i++)
+{
+if(j%i==0)
+{
+flag=0;
+break;
+}
+}
+if(flag==1)
+{
+System.out.println("\nPrime number:"+j);
+count++;
+}
+j++;
+Thread.sleep(500);
+}
+}
+catch(InterruptedException e)
+{
+System.out.println("\nPrime Thread Interrupted!");
+}
+}
+}
+class Threads
+{
+public static void main(String[]args)
+{
+Multiplication mul=new Multiplication();
+Scanner sc=new Scanner(System.in);
+System.out.println("\nEnter the limit :");
+int l=sc.nextInt();
+Prime p=new Prime(l);
+mul.start();
+p.start();
+}
+}
